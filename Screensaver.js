dbb4f286fb6f6c662dc84dbece9b18f85ccaedd7
Jose Prado <software@joseprado.com>
2009-11-18 19:11:55 -0600

    App Commit

---
+++ Screensaver.js
@@ -0,0 +1,54 @@
+/**
+ * Script:
+ *   Screensaver.js - Screensaver functionality written in MooTools.
+ *
+ * License:
+ *   MIT-style license.
+ * 
+ * Author:
+ *   Jose Prado
+ *
+ * Copyright:
+ *   Copyright (©) 2009 [Jose Prado](http://pradador.com/).
+ *
+ */
+var Screensaver = new Class({
+	Implements: [Options, Events],
+	options: {/**
+		onIdle: $empty,
+		onActive: $empty,*/
+		events: ['click', 'keypress', 'mousemove', 'resize', 'scroll'],
+		target: window,
+		ttl: 10000
+	},
+
+	initialize: function(options) {
+		this.setOptions(options);
+		this.enabled = false;
+
+		var target = $(this.options.target);
+		this.options.events.each(function(ev) {
+			target.addEvent(ev, this.reset.bind(this));
+		}, this);
+
+		this.monitor();
+	},
+
+	monitor: function() {
+		this.timer = this.display.delay(this.options.ttl, this);
+	},
+
+	reset: function() {
+		$clear(this.timer);
+		if (this.enabled) {
+			this.fireEvent('active');
+			this.enabled = false;
+		}
+		this.monitor();
+	},
+
+	display: function() {
+		this.fireEvent('idle');
+		this.enabled = true;
+	}
+});
